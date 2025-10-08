f859de5902fb083ecc9b1708fc5956d24aab0d2c
ayuwardatul <ayuwardatulww@gmail.com>
2025-10-08 11:01:35 +0700

    percobaan 1

--- jobsheet5/ifCetakKRS04.java
+++ jobsheet5/ifCetakKRS04.java
@@ -1,14 +1,16 @@
+import java.util.Scanner;
+
 public class ifCetakKRS04 {
     public static void main(String[] args) {
-        Scanner sc04 = new Scanner (System.in);
+        Scanner sc04 = new Scanner(System.in);
 
-        System.out.pritntln("---Cetak KRS SIAKAD---");
-        System.out.pritntln("Apakah UKT sudah lunas? (true/false):");
+        System.out.println("---Cetak KRS SIAKAD---");
+        System.out.println("Apakah UKT sudah lunas? (true/false):");
         boolean uktLunas = sc04.nextBoolean();
 
         if (uktLunas) {
-             System.out.pritntln("pembayaran UKT terlunasi");
-              System.out.pritntln("Silahkan cetak kRS dan minta tanda tangan DPA");
+             System.out.println("pembayaran UKT terlunasi");
+              System.out.println("Silahkan cetak kRS dan minta tanda tangan DPA");
               
         }
     }
