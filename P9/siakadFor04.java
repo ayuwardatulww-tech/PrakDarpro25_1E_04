a9d5c6e8e780ce0fe3d803e820127ffe6626393f
Ayu Wardatul Widad <ayuwardatulwwu@gmail.com>
2025-10-28 15:23:27 +0700

    P9_percobaan1_pertanyaan4

--- P9/siakadFor04.java
+++ P9/siakadFor04.java
@@ -6,6 +6,7 @@ public class siakadFor04 {
     public static void main(String[] args) {
         Scanner sc04 = new Scanner(System.in);
         double nilai04, tertinggi04=0, terendah04=100;
+        int batasNilai04=60, lulus04=0, tidaklulus04 =0; 
 
         for (int i =1; i <=10; i++) {
             System.out.print("Masukkan nilai mahasiswa ke- "+i+":");
@@ -15,6 +16,13 @@ public class siakadFor04 {
             }
             if (nilai04<terendah04) {
                 terendah04 = nilai04;
+            }
+            if (nilai04>batasNilai04) {
+                nilai04 = lulus04;
+                lulus04++;
+            }else {
+                nilai04 = tidaklulus04;
+                tidaklulus04++;
             }
         
         }
