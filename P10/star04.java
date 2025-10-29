9e83e26826ad8015bdd3bfa98773d1d3178ad335
Ayu Wardatul Widad <ayuwardatulwwu@gmail.com>
2025-10-29 12:12:16 +0700

    P10_Percobaan 1

---
+++ P10/star04.java
@@ -0,0 +1,16 @@
+package P10;
+
+import java.util.Scanner;
+
+public class star04 {
+    public static void main(String[] args) {
+        Scanner ayu = new Scanner(System.in);
+        System.out.print("Masukkan nilai n =");
+        int n = ayu.nextInt();
+
+        for (int i = 1; i <= n ; i++2) {
+            System.out.print("*");
+        }
+
+    }
+}
