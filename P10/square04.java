f4c611cfeb1b367b57a0cfe0216aef041e474ee0
Ayu Wardatul Widad <ayuwardatulwwu@gmail.com>
2025-10-29 12:33:54 +0700

    P10_Percobaan2

---
+++ P10/square04.java
@@ -0,0 +1,18 @@
+package P10;
+
+import java.util.Scanner;
+
+public class square04 {
+    public static void main(String[] args) {
+        Scanner ayu = new Scanner(System.in);
+        System.out.print("Masukkan nilai N = ");
+        int N = ayu.nextInt();
+        for (int iOuter = 1; iOuter <=N; iOuter++) {
+            for (int i = 1; i <=N; i++) {
+            System.out.print("*");
+            }
+            
+        }
+       
+    }
+}
