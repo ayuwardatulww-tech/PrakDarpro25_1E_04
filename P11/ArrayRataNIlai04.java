c84a34cca4c2f71145e976af8d2a1d361788a957
Ayu Wardatul Widad <ayuwardatulwwu@gmail.com>
2025-11-10 09:11:40 +0700

    P11_Percobaan 3_Pertanyaan 2

--- P11/ArrayRataNIlai04.java
+++ P11/ArrayRataNIlai04.java
@@ -6,10 +6,13 @@ public class ArrayRataNIlai04 {
     public static void main(String[] args) {
         Scanner ayu = new Scanner(System.in);
 
-        int [] nilaiMhs04 = new int [10];
+        System.out.print("Masukkan jumlah mahasiswa : ");
+        int jmlMhs04 = ayu.nextInt();
+
+        int [] nilaiMhs04 = new int [jmlMhs04];
         double total04 = 0;
         double rata04;
-        int totLulus04=0;
+        double totLulus04=0, totNOLulus =0, jmlLulus=0, jmlNoLulus=0;
 
         for (int i = 0; i < nilaiMhs04.length; i++) {
             System.out.print("Masukkan nilai mahasiswa ke-"+(i+1)+" : ");
@@ -17,15 +20,22 @@ public class ArrayRataNIlai04 {
         }
         for (int i = 0; i < nilaiMhs04.length; i++) {
             if (nilaiMhs04[i]>70) {
-                System.out.println("Mahasiswa ke-"+i+" lulus");
-                totLulus04++;
-            } 
+                totLulus04 += nilaiMhs04[i];
+                jmlLulus++;
+            } else {
+                totNOLulus += nilaiMhs04[i];
+                jmlNoLulus++;
+            }
         }
-        for (int i = 0; i < nilaiMhs04.length; i++) {
-            total04 += nilaiMhs04[i];
-        }
+       
         rata04 = total04/nilaiMhs04.length; 
+        double rataLulus = totLulus04/jmlLulus;
+        double rataNoLulus = totNOLulus/jmlNoLulus;
+        
         System.out.println("Rata - rata nilai = "+rata04);
-        System.out.println("Total mahasiswa lulus : "+totLulus04 );
-    }
+        System.out.println("Rata - rata nilai lulus = " + rataLulus);
+        System.out.println("Rata - rata nilai tidak lulus = " + rataNoLulus );
+        
+        
+}
 }
