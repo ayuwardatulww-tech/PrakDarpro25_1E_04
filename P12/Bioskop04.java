1f72a3265a956d9f3e2e91b8fb7da1860f327fbd
Ayu Wardatul Widad <ayuwardatulwwu@gmail.com>
2025-11-12 11:11:31 +0700

    P12_Percobaan 1

---
+++ P12/Bioskop04.java
@@ -0,0 +1,20 @@
+package P12;
+public class Bioskop04 {
+    public static void main(String[] args) {
+        String [][] penonton04 = new String[4][2];
+        penonton04[0][0] = "Amin";
+        penonton04[0][1] = "Bena";
+        penonton04[1][0] = "Candra";
+        penonton04[1][1] = "Dela";
+        penonton04[2][0] = "Eka";
+        penonton04[2][1] = "Farhan";
+        penonton04[3][0] = "Gisel";
+
+        System.out.printf("%s \t %s \n", penonton04[0][0], penonton04[0][1]);
+        System.out.printf("%s \t %s \n", penonton04[1][0], penonton04[1][1]);
+        System.out.printf("%s \t %s \n", penonton04[2][0], penonton04[2][1]);
+        System.out.printf("%s \t %s \n", penonton04[3][0], penonton04[3][1]);
+
+
+    }
+}
