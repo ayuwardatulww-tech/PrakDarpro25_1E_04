682e6f9e54a66cfdfd0ebdb8b65c3e91b87738ce
Ayu Wardatul Widad <ayuwardatulwwu@gmail.com>
2025-11-15 12:45:20 +0700

    P12_Percobaan 3_Pertanyaan 4

--- P12/Numbers04.java
+++ P12/Numbers04.java
@@ -12,5 +12,8 @@ public class Numbers04 {
         for (int i = 0; i < myNumbers04.length; i++) {
             System.out.println(Arrays.toString(myNumbers04[i]));
         }
+        for (int i = 0; i < myNumbers04.length; i++) {
+            System.out.println("Panjang baris ke-"+ (i+1)+ " : "+ myNumbers04[i].length);
+        }
     }
 }
