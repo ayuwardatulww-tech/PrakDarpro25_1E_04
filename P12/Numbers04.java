724bc8a2354a776049e34942ac8e395e84b5224b
Ayu Wardatul Widad <ayuwardatulwwu@gmail.com>
2025-11-15 12:30:43 +0700

    P12_Percobaan 3_Pertanyaan 1

--- P12/Numbers04.java
+++ P12/Numbers04.java
@@ -1,11 +1,16 @@
 package P12;
 
+import java.util.Arrays;
+
 public class Numbers04 {
     public static void main(String[] args) {
         int [][] myNumbers04 = new int[3][];
         myNumbers04[0] = new int[5];
         myNumbers04[1] = new int[3];
         myNumbers04[2] = new int[1];
-
+        
+        for (int i = 0; i < myNumbers04.length; i++) {
+            System.out.println(Arrays.toString(myNumbers04[i]));
+        }
     }
 }
