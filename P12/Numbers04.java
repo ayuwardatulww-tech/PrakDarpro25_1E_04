2f48d78beb2907df2048edb7c20ae55fa9f8a1b9
Ayu Wardatul Widad <ayuwardatulwwu@gmail.com>
2025-11-15 18:32:10 +0700

    P12_Percobaan 3_Pertanyaan 1

--- P12/Numbers04.java
+++ P12/Numbers04.java
@@ -4,16 +4,15 @@ import java.util.Arrays;
 
 public class Numbers04 {
     public static void main(String[] args) {
+        
         int [][] myNumbers04 = new int[3][];
         myNumbers04[0] = new int[5];
         myNumbers04[1] = new int[3];
         myNumbers04[2] = new int[1];
-        
+
         for (int i = 0; i < myNumbers04.length; i++) {
             System.out.println(Arrays.toString(myNumbers04[i]));
         }
-        for (int i = 0; i < myNumbers04.length; i++) {
-            System.out.println("Panjang baris ke-"+ (i+1)+ " : "+ myNumbers04[i].length);
-        }
+        
     }
 }
