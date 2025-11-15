8b22123838d1faa98a0f5a3d21dcbf3dd2cdde35
Ayu Wardatul Widad <ayuwardatulwwu@gmail.com>
2025-11-15 12:22:57 +0700

    P12_Percobaan 3

---
+++ P12/Numbers04.java
@@ -0,0 +1,11 @@
+package P12;
+
+public class Numbers04 {
+    public static void main(String[] args) {
+        int [][] myNumbers04 = new int[3][];
+        myNumbers04[0] = new int[5];
+        myNumbers04[1] = new int[3];
+        myNumbers04[2] = new int[1];
+
+    }
+}
