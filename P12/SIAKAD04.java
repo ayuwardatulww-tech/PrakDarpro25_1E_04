4e74be0c9bb998716bcff5739fc9afcf2c553380
Ayu Wardatul Widad <ayuwardatulwwu@gmail.com>
2025-11-16 11:28:58 +0700

    P12_Percobaan 4_Modifikasi

--- P12/SIAKAD04.java
+++ P12/SIAKAD04.java
@@ -9,11 +9,25 @@ public class SIAKAD04 {
         int [][] nilai04 = new int[4][3];
         for (int i = 0; i < nilai04.length; i++) {
             System.out.println("Input nilai mahasiswa ke-"+ (i+1));
+            double totalPerSiswa04 =0;
 
             for (int j = 0; j < nilai04[i].length; j++) {
                 System.out.print("Nilai mata kuliah "+ (j+1)+ " : ");
                 nilai04[i][j] = ayu.nextInt();
+                totalPerSiswa04 += nilai04[i][j];
             }
+            System.out.println("Nilai rata - rata : "+(int)totalPerSiswa04/3);
+        }
+        System.out.println("\n====================================");
+        System.out.println("Rata rata Nilai setiap Mata Kuliah : ");
+
+        for (int j = 0; j < 3; j++) {
+            double totalPerMatkul04 = 0;
+
+            for (int i = 0; i < 4; i++) {
+                //totalPerMatkul04 += nilai04[i][j];
+            }
+            System.out.println("Mata kuliah "+(j+1)+": "+totalPerMatkul04/4);
         }
     }
 }
