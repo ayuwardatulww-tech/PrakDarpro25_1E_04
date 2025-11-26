f396019ecabc017acbb1b1b822ea871c8b9a7187
Ayu Wardatul Widad <ayuwardatulwwu@gmail.com>
2025-11-26 14:26:48 +0700

    P14_Percobaan 1

---
+++ P14/Kafe04.java
@@ -0,0 +1,16 @@
+public class Kafe04 {
+    public static void main(String[] args) {
+        Menu04();
+    }
+    public static void Menu04(){
+        System.out.println("===== MENU RESTO KAFE =====");
+        System.out.println("1. KOpi Hitam - RP. 15.000");
+        System.out.println("2. Cappucino - RP.20.000");
+        System.out.println("3. Latte - Rp.22.000");
+        System.out.println("4. Teh Tarik - Rp. 12.000");
+        System.out.println("5. Roti Bakar - Rp. 10.000");
+        System.out.println("6. Mie Goreng - Rp. 18.000");
+        System.out.println("=================================");
+        System.out.println("Silahkan Pilih Menu Yang Anda Ingingkan ");
+    }
+}
