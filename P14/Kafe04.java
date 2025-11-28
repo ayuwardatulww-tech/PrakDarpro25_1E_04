ea71e4b3bdde621331fc9a7d095c9d9769b01d5b
Ayu Wardatul Widad <ayuwardatulwwu@gmail.com>
2025-11-28 19:02:04 +0700

    P14_Percobaan 3_PertanyaanNo3

--- P14/Kafe04.java
+++ P14/Kafe04.java
@@ -7,31 +7,35 @@ public class Kafe04 {
     public static String DISKON30 ="DISKON30";
     public static void main(String[] args) {
         Scanner ayu = new Scanner(System.in);
-        Menu04("Budi", true, DISKON30);
+        Menu04("Budi", true);
 
-        System.out.print("\nMasukkan nomor menu yang ingin anda pesan:");
+        System.out.print("\nMasukkan nomor menu yang ingin anda pesan : ");
         int pilihanMenu04 = ayu.nextInt();
-        System.out.print("Masukkan jumlah item yang ingin dipesan:");
+        System.out.print("Masukkan jumlah item yang ingin dipesan : ");
         int banyakItem04 = ayu.nextInt();
+        ayu.nextLine();
+        System.out.print("Masukkan kode promo : ");
+        String kodePromo04 = ayu.nextLine();
 
-        int total04 = HitungTotalharga04(pilihanMenu04,banyakItem04);
+        int total04 = HitungTotalharga04(pilihanMenu04,banyakItem04,kodePromo04);
         System.out.println("Total bayar : Rp."+ total04);
     }
-    public static int HitungTotalharga04 (int pilihanMenu04, int banyakItem04) {
+    public static int HitungTotalharga04 (int pilihanMenu04, int banyakItem04, String kodePromo04) {
+        if (kodePromo04.equalsIgnoreCase(DISKON50)) {
+            System.out.println("Anda mendapatkan diskon 50%");
+        }else if (kodePromo04.equalsIgnoreCase(DISKON30)) {
+            System.out.println("Anda mendapatkan diskon 30%");
+        }else {
+            System.out.println("Tampilan kode invalid");
+        }
         int[] hargaItems = {15000, 20000, 22000, 12000, 18000, 18000};
 
         int hargaTotal04 = hargaItems[pilihanMenu04-1]*banyakItem04;
         return hargaTotal04;
     }
-    public static void Menu04(String namaPelanggan04, boolean isMember04, String kodePromo04){
+    public static void Menu04(String namaPelanggan04, boolean isMember04){
         if (isMember04) {
             System.out.println("Anda adalah member dapatkan diskon 10% untuk setiap pembelian!");
-        }if (kodePromo04==DISKON50) {
-            System.out.println("Anda mendapatkan diskon 50%");
-        }else if (kodePromo04==DISKON30) {
-            System.out.println("Anda mendapatkan diskon 30%");
-        }else {
-            System.out.println("Tampilan kode invalid");
         }
         System.out.println("===== MENU RESTO KAFE =====");
         System.out.println("1. KOpi Hitam - RP. 15.000");
