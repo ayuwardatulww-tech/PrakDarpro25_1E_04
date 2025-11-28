59f9807267985f5a2236b29872df769afcad3def
Ayu Wardatul Widad <ayuwardatulwwu@gmail.com>
2025-11-28 08:36:30 +0700

    P14_Percobaan 2_PertanyaanNo6

--- P14/Kafe04.java
+++ P14/Kafe04.java
@@ -1,13 +1,16 @@
 package P14;
 public class Kafe04 {
+    public static String DISKON50 ="DISKON50";
+    public static String DISKON30 ="DISKON30";
+    
     public static void main(String[] args) {
-        Menu("Andi", true);
+        Menu04("BUdi", true);
+        Diskon04("DISKON50");
     }
-        public static void Menu(String namaPelanggan04, boolean isMember04){
+        public static void Menu04(String namaPelanggan04, boolean isMember04){
             if (isMember04) {
                 System.out.println("Anda adalah member dapatkan diskon 10% untuk setiap pembelian!");
             }
-        
         System.out.println("===== MENU RESTO KAFE =====");
         System.out.println("1. KOpi Hitam - RP. 15.000");
         System.out.println("2. Cappucino - RP.20.000");
@@ -18,4 +21,14 @@ public class Kafe04 {
         System.out.println("=================================");
         System.out.println("Silahkan Pilih Menu Yang Anda Ingingkan ");
     }
+    public static void Diskon04(String kodePromo){
+        if (kodePromo==DISKON50) {
+            System.out.println("Anda mendapatkan diskon 50%");
+        }else if (kodePromo==DISKON30) {
+            System.out.println("Anda mendapatkan diskon 30%");
+        }else {
+            System.out.println("Tampilan kode invalid");
+        }
+    }
+
 }
