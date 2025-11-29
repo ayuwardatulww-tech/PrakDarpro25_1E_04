dd63f41a50776b661b3fc7c0737d70d825a9c5ab
Ayu Wardatul Widad <ayuwardatulwwu@gmail.com>
2025-11-29 09:10:13 +0700

    P14_Percobaan 4_PertanyaanNo.4

--- P14/PengunjungCafe04.java
+++ P14/PengunjungCafe04.java
@@ -1,9 +1,10 @@
 package P14;
 public class PengunjungCafe04 {
     public static void main(String[] args) {
-        daftarPengunjung04("Ali","Budi","Citra"); 
-        daftarPengunjung04("Andi");
-        daftarPengunjung04("Doni","Eti","Fahmi","Galih");
+        // daftarPengunjung04("Ali","Budi","Citra"); 
+        // daftarPengunjung04("Andi");
+        // daftarPengunjung04("Doni","Eti","Fahmi","Galih");
+        daftarPengunjung04(args);
     }
 
     static void daftarPengunjung04 (String...namaPengunjung04){
