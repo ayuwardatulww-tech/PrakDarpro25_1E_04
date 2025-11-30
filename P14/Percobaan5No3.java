23e087dc9cdb48290c3203dae1dfeae5595c95a1
Ayu Wardatul Widad <ayuwardatulwwu@gmail.com>
2025-11-30 14:38:48 +0700

    P14_Percobaan 5_PertanyaanNo3

---
+++ P14/Percobaan5No3.java
@@ -0,0 +1,18 @@
+package P14;
+public class Percobaan5No3 {
+    public static void TampilHInggaKei(int i){
+        for (int j = 1; j <= i; j++) {
+            System.out.println(j);
+        }
+    }
+    public static int Jumlah (int bil1, int bil2){
+        return (bil1 + bil2);
+    }
+    public static void TampilJumlah (int bil1, int bil2){
+        TampilHInggaKei(Jumlah(bil1, bil2));
+    }
+    public static void main(String[] args) {
+        int temp = Jumlah(1,1);
+        TampilJumlah(temp, 5);
+    }
+}
